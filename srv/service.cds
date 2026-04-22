27044310cbe58dad7cc1b1034ea8d0ebbd9e037c
Naim Daoud <naim.daoud@qactionsystem.com>
2026-04-22 20:44:54 +0000

    agrego logica de la fecha del turno debe ser mayor al dia actual y el horario laboral

---
+++ srv/service.cds
@@ -0,0 +1,13 @@
+using { consultorio.hospital as my } from '../db/schema.cds';
+
+@path : '/service/ConsultoriaMedicaService'
+service ConsultoriaMedicaService
+{
+    entity Turnos as projection on my.Turno;
+    entity Pacientes as projection on my.Paciente;
+    entity Doctores as projection on my.Doctor;
+}
+
+annotate ConsultoriaMedicaService with @requires : [
+    'authenticated-user'
+];
---
+++ srv/service.js
@@ -0,0 +1,25 @@
+const cds = require('@sap/cds')
+
+module.exports = (srv) => {
+    const { Turnos } = srv.entities
+
+    srv.before('CREATE', 'Turnos', (req) => {
+        const { fechaHora } = req.data
+        
+        if (fechaHora) {
+            const hoy = new Date().toISOString()
+            const fechaTurnoDate = new Date(fechaHora)
+            const hora = fechaTurnoDate.getHours()
+
+            // 1. Validación: Que no sea en el pasado
+            if (fechaHora < hoy) {
+                return req.error(400, `Error, No podes agendar un turno para el pasado (${fechaHora}).`)
+            }
+
+            // 2. Validación: Horario laboral (06:00 a 18:00)
+            if (hora >= 18 || hora < 6) {
+                return req.error(400, 'El horario debe estar dentro de la jornada laboral (06:00 a 18:00)')
+            }
+        }
+    })
+}
